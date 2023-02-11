88ac08999efd0fc76b8268d5e0ec668b85c7db26
Lucia <luciamigliorati01@gmail.com>
2023-02-11 19:27:41 +0100

    script definitivo

--- mdl.py
+++ mdl.py
@@ -1,4 +1,12 @@
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from tqdm import tqdm
+from scipy import constants, optimize
+import scipy.fft
+
+
 class Stazione():
     """
     Classe per rappresentare le stazioni di monitoraggio degli inquinanti
@@ -10,6 +18,9 @@ class Stazione():
     contea            : contea di monitoraggio 
     città             : città di monitoraggio 
     date  array       : date di campionamento
+    lunghezza date    : lunghezza array date
+    prima data        : data di inzio monitoraggio
+    ultima data       : data di fine monitoraggio
     medie NO2         : valor medio del diossido di azoto rilevato dalla stazione di monitoraggio nelle date di campionamento
     medie O3          : valor medio dell'ozono rilevato dalla stazione di monitoraggio nelle date di campionamento
     medie SO2         : valor medio dell'anidride solforosa rilevata dalla stazione di monitoraggio nelle date di campionamento
@@ -22,49 +33,28 @@ class Stazione():
     
     def __init__(self):
         self.numeroStazione = 0
-        self.stato    = ''
-        self.contea   = ''
-        self.città    = ''
-        self.dateArray= np.empty(0)
-        self.medieNO2 = np.empty(0)
-        self.medieO3  = np.empty(0)
-        self.medieSO2 = np.empty(0)
-        self.medieCO  = np.empty(0)
-        self.lunghezzaDate= len(self.dateArray)
+        self.stato     = ''
+        self.contea    = ''
+        self.città     = ''
+        self.dateArray = np.empty(0)
+        self.lunghezzaDate = 0
+        self.primaData = 0
+        self.ultimaData= 0
+        self.medieNO2  = np.empty(0)
+        self.medieO3   = np.empty(0)
+        self.medieSO2  = np.empty(0)
+        self.medieCO   = np.empty(0)
 
     def aggiungi_stazione(self, stato, dataLocale, numeroStazioni, città, contea, NO2medie, O3medie, SO2medie, COmedie):
         self.numeroStazione = numeroStazioni
         self.stato = stato     
         self.contea = contea
         self.città = città
-        self.dateArray = dataLocale
-        self.medieNO2 = NO2medie
-        self.medieO3 = O3medie
-        self.medieSO2 = SO2medie
-        self.medieCO = COmedie
-
-'''
-    
-    def stampa_stazione(numeroStazione):
-        print('Numero stazione /n', self.numeroStazione)
-        print('New York /n', self.stato)
-        print('Contea /n', self.contea)
-        print('Città /n', self.città)
-        print('Medie giornaliere NO2 /n', self.dateArray, '  ', self.medieNO2)
-        print('Medie giornaliere O3 /n', self.dateArray, '  ', self.medieO3)
-        print('Medie giornaliere SO2 /n', self.dateArray, '  ', self.medieSO2)
-        print('Medie giornaliere CO /n', self.dateArray, '  ' ,self.medieCO)
-        
-    def __eq__(self, other):
-        return  self.ruote == other.ruote and self.potenza == other.potenza
-
-
-    def __lt__(self, other):
-        return self.potenza < other.potenza
-
-    def __gt__(self, other):
-        return self.potenza > other.potenza
-    
-    def minore(self, other):
-        return self.potenza < other.potenza  
-'''
+        self.dateArray = np.append(self.dateArray, dataLocale)
+        self.lunghezzaDate = len(self.dateArray)
+        self.primaData = np.sort(self.dateArray)[0]
+        self.ultimaData = np.sort(self.dateArray)[-1]
+        self.medieNO2 = np.append(self.medieNO2, NO2medie)
+        self.medieO3 = np.append(self.medieO3, O3medie)
+        self.medieSO2 = np.append(self.medieSO2, SO2medie)
+        self.medieCO = np.append(self.medieCO, COmedie)
